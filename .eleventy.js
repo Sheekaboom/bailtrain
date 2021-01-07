b8390a829812606eafd536e1059919fa7d6a1ae6
Sheekaboom <aweiss7791@gmail.com>
2021-01-07 13:02:58 -0700

    changes for qorking site

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,6 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.setTemplateFormats([
-      "md",
-      "css" // css is not yet a recognized template extension in Eleventy
-    ]);
-
-    eleventyConfig.addPassthroughCopy("*.jpg");
-
     return {
+      templateFormats: ["html","md"],
       dir:{
         input:'site'
       }
